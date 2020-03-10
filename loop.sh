86999e895394abb0ef0b2d6c9bd8996df28f06c1
Jake Tivy <sales@tivy.ca>
2020-03-09 20:22:27 -0700

    add utf-8 flag

--- loop.sh
+++ loop.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 while true; do
     sh update.sh
-    java -server -Xms8G -Xmx8G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=35 -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -Dusing.aikars.flags=mcflags.emc.gs -jar paperclip.jar
+    java -server -Xms8G -Xmx8G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=35 -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -Dfile.encoding=UTF-8 -jar paperclip.jar
     echo "If you want to completely stop the server process now, press Ctrl+C before the time is up!"
     echo "Rebooting in:"
     for i in 5 4 3 2 1; do
