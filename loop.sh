9d307bea90e1f3997371c9bb4372206455717217
Jake Tivy <sales@tivy.ca>
2020-03-17 17:39:17 -0700

    decrease restarting wait to 3 seconds, from 5s

--- loop.sh
+++ loop.sh
@@ -4,7 +4,7 @@ while true; do
     java -server -Xms8G -Xmx8G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=35 -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -Dfile.encoding=UTF-8 -jar paperclip.jar
     echo "If you want to completely stop the server process now, press Ctrl+C before the time is up!"
     echo "Rebooting in:"
-    for i in 5 4 3 2 1; do
+    for i in 3 2 1; do
         echo "$i..."
         sleep 1
     done
